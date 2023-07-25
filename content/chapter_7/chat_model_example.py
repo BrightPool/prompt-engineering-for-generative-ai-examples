fbaf68550a444e100adfe253a3fbf4583f3785ec
jamesaphoenix <jamesaphoenix@googlemail.com>
2023-07-25 16:22:39 +0100

    Working on the vector stores section.

--- content/chapter_7/chat_model_example.py
+++ content/chapter_7/chat_model_example.py
@@ -1,6 +1,6 @@
 import asyncio
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.schema import HumanMessage, SystemMessage
 
 chat = ChatOpenAI(temperature=0)  # type: ignore
 messages = [
