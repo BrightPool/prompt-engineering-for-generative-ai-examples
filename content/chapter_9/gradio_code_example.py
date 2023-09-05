b7340cd4d93115d21182fb6046c51db162ad7260
jamesaphoenix <jamesaphoenix@googlemail.com>
2023-09-05 14:56:45 +0100

    Debugging the gradio code.

--- content/chapter_9/gradio_code_example.py
+++ content/chapter_9/gradio_code_example.py
@@ -14,15 +14,23 @@ from article_generation import ContentGenerator
 
 os.environ["SERPAPI_API_KEY"] = ""
 
-import gradio as gr
+
+question_boxes = []
+answer_boxes = []
+
+
+def test():
+    print("hello world")
+    print(dir(question_boxes[0]))
+    question_boxes[0].value = "Test"
+    question_boxes[0].update(value="Test", visible=True)
+    question_boxes[0].input.value = "Test"
 
 
 def get_summary(topic):
-    # Create a new event loop for this task
     new_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(new_loop)
 
-    # Now run the asynchronous function until completion
     try:
         result = new_loop.run_until_complete(async_get_summary(topic))
     finally:
@@ -42,16 +50,37 @@ async def async_get_summary(topic):
     )
     parser = PydanticOutputParser(pydantic_object=DocumentSummary)
     # Create the summaries:
+    print("Creating all of the summaries...\n---" "")
     summaries = await create_all_summaries(text_documents, parser, llm, text_splitter)
 
     # Create interview questions:
+    print("Creating the interview questions...\n---" "")
     interview_chain = InterviewChain(topic=topic, document_summaries=summaries)
     interview_questions = interview_chain()
 
-    return text_documents, summaries, interview_questions
+    # Update the gradio UI:
+    for i, q in enumerate(interview_questions.questions):
+        question_boxes[i].update(value=q.question, visible=True)
+        answer_boxes[i].update(visible=True)
+
+    return summaries, interview_questions
 
 
-def generate_content(
+def generate_content(topic, summaries, text_documents):
+    new_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(new_loop)
+
+    try:
+        result = new_loop.run_until_complete(
+            async_generate_content(topic, summaries, text_documents)
+        )
+    finally:
+        new_loop.close()
+
+    return result
+
+
+async def async_generate_content(
     topic,
     text_documents,
     summaries,
@@ -77,60 +106,69 @@ def generate_content(
 
 with gr.Blocks() as demo:
     with gr.Row():
-        topic = gr.Textbox(label="Topic", scale=70, value="Memetics")
-        mock_question = gr.outputs.Textbox(
-            label="Question from Tool"
-        )  # Display the tool's question
-        user_answer = gr.Textbox(label="Your Answer", scale=10)  # Accept user's answer
-
-    with gr.Row():
         topic = gr.Textbox(label="Topic", scale=85, value="Memetics")
-        summarize_btn = gr.Button("Summarize", scale=15)
+        summarize_btn = gr.Button("Summarize and Generate Questions", scale=15)
+        test_btn = gr.Button("Test", scale=15)
 
     with gr.Row():
         summaries = gr.Textbox(label="Summary", lines=10)
-
-    with gr.Row():
         interview_questions = gr.Textbox(label="Questions", lines=20)
-        interview_answers = gr.Textbox(label="Answers", lines=20)
-        text_documents = gr.Textbox(label="Text Documents", lines=20)
 
     with gr.Row():
         summarize_btn.click(
             fn=get_summary,
             inputs=[topic],
-            outputs=[summaries, interview_questions, text_documents],
+            outputs=[summaries, interview_questions],
         )
-
-        clear_btn = gr.Button("Clear", scale=15)
-        generate_btn = gr.Button("Generate", scale=30)
 
     with gr.Row():
-        with gr.Column():
-            generated_content = gr.Textbox(label="Content", lines=50)
-            generated_image = gr.Image(shape=(1200, 630))
-            generated_prompt = gr.Textbox(label="Prompt")
-
-        generate_btn.click(
-            fn=generate_content,
-            inputs=[topic, summaries, text_documents],  # Changed from 'summary_text'
-            outputs=[generated_content, generated_image, generated_prompt],
-        )
-
-        # Reset the UI elements to default
-        clear_btn.click(
-            fn=lambda: ("", "", "", "", "", "", None, ""),  # Matching the output count
+        test_btn.click(
+            fn=test,
             inputs=[],
-            outputs=[
-                topic,
-                summaries,
-                text_documents,
-                interview_questions,
-                interview_answers,
-                generated_content,
-                generated_image,
-                generated_prompt,
-            ],
+            outputs=[],
         )
+
+    with gr.Column(scale=4):  # This column will take up 80% of the width
+        question_boxes = []
+        answer_boxes = []
+
+        for i in range(5):
+            with gr.Row():
+                question = gr.Textbox(
+                    label=f"Question {i + 1}", lines=1, visible=True, interactive=False
+                )
+                answer = gr.Textbox(label=f"Answer {i + 1}", lines=5, visible=True)
+                question_boxes.append(question)
+                answer_boxes.append(answer)
+
+    # clear_btn = gr.Button("Clear", scale=15)
+    # generate_btn = gr.Button("Generate", scale=30)
+
+    # with gr.Row():
+    #     with gr.Column():
+    #         generated_content = gr.Textbox(label="Content", lines=50)
+    #         generated_image = gr.Image(shape=(1200, 630))
+    #         generated_prompt = gr.Textbox(label="Prompt")
+
+    #     generate_btn.click(
+    #         fn=generate_content,
+    #         inputs=[topic, summaries, text_documents],  # Changed from 'summary_text'
+    #         outputs=[generated_content, generated_image, generated_prompt],
+    #     )
+
+    #     # Reset the UI elements to default
+    #     clear_btn.click(
+    #         fn=lambda: ("", "", "", "", "", None, ""),  # Matching the output count
+    #         inputs=[],
+    #         outputs=[
+    #             topic,
+    #             summaries,
+    #             text_documents,
+    #             interview_questions,
+    #             generated_content,
+    #             generated_image,
+    #             generated_prompt,
+    #         ],
+    #     )
 
 demo.launch()
