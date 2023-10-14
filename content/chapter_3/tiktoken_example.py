82803aaa43d6f6698f62004ce092e7b83c1ec73e
jamesaphoenix <jamesaphoenix@googlemail.com>
2023-10-14 20:21:53 +0100

    Updating the repo for chapter 3.

--- content/chapter_3/tiktoken_example.py
+++ content/chapter_3/tiktoken_example.py
@@ -31,3 +31,83 @@ def count_tokens(text_string: str, encoding_name: str) -> int:
 # 4. Use the function to count the number of tokens in a text string.
 text_string = "Hello world! This is a test."
 print(count_tokens(text_string, "cl100k_base"))
+
+
+# Use this function to count the number of tokens in a list of messages:
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+
+example_messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful, pattern-following assistant that translates corporate jargon into plain English.",
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "New synergies will help drive top-line growth.",
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": "Things working well together will increase revenue.",
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "Let's circle back when we have more bandwidth to touch base on opportunities for increased leverage.",
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": "Let's talk later when we're less busy about how to do better.",
+    },
+    {
+        "role": "user",
+        "content": "This late pivot means we don't have time to boil the ocean for the client deliverable.",
+    },
+]
+
+for model in ["gpt-3.5-turbo-0301", "gpt-4-0314"]:
+    print(model)
+    # example token count from the function defined above
+    print(f"{num_tokens_from_messages(example_messages, model)} prompt tokens counted by num_tokens_from_messages().")
